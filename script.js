84ec54a0355a6675b9e13ce81843ca4a6946f86c
Praise-A <praise.amaraegbulam@stu.cu.edu.ng>
2023-08-06 19:17:53 +0100

    Add button and functions for clearing the game and starting a new round

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let rockBtn = document.querySelector("#rockBtn");
 let paperBtn = document.querySelector("#paperBtn");
 let scissorsBtn = document.querySelector("#scissorsBtn");
+let winnerComment = document.querySelector("#winnerComment");
+let computerComment = document.querySelector("#desc");
+let overlay = document.querySelector(".overlay");
+let restartBtn = document.querySelector("#restartBtn");
+
+let pScore = document.querySelector("#playerScore");
+let cScore = document.querySelector("#computerScore");
 
 let playerScore = 0;
 let computerScore = 0;
@@ -13,28 +20,18 @@ const getComputerChoice = (selection) => {
 
 //Play Round
 const playRound = (player, computer) => {
-  if (player === "rock" && computer === "paper") {
-    computerScore++;
-    return "Paper covers Rock; Computer Wins";
-  } else if (player === "paper" && computer === "scissors") {
-    computerScore++;
-    return "Scissors cuts Paper; Computer Wins";
-  } else if (player === "scissors" && computer === "rock") {
-    computerScore++;
-    return "Rock breaks Scissors; Computer Wins";
-  } else if (player === "rock" && computer === "scissors") {
+  if (
+    (player === "rock" && computer.toLowerCase() === "scissors") ||
+    (player === "paper" && computer.toLowerCase() === "rock") ||
+    (player === "scissors" && computer.toLowerCase() === "paper")
+  ) {
     playerScore++;
-    return "Rock breaks Scissors; You Win";
-  } else if (player === "paper" && computer === "rock") {
-    playerScore++;
-    return "Paper covers Rock; You Win";
-  } else if (player === "scissors" && computer === "paper") {
-    playerScore++;
-    return "Scissors cuts Paper; You Win";
-  } else if (player === computer) {
-    return "It's a tie! Play again";
+    return "You Win";
+  } else if (player === computer.toLowerCase()) {
+    return "It's a tie!";
   } else {
-    return console.error();
+    computerScore++;
+    return "Computer Wins";
   }
 };
 
@@ -44,22 +41,47 @@ const playRound = (player, computer) => {
 rockBtn.addEventListener("click", () => assignValue("rock"));
 paperBtn.addEventListener("click", () => assignValue("paper"));
 scissorsBtn.addEventListener("click", () => assignValue("scissors"));
+restartBtn.addEventListener("click", () => restartGame());
 
 const assignValue = (playerSelection) => {
-  const selection = ["rock", "paper", "scissors"];
+  if (playerScore === 5) {
+    winnerComment.innerHTML = "<h1>Player Wins The Game</h1>";
+    endGame();
+  } else if (computerScore === 5) {
+    winnerComment.innerHTML = "<h1>Computer Wins The Game</h1>";
+    endGame();
+  }
+
+  const selection = ["Rock", "Paper", "Scissors"];
   const computerSelection = getComputerChoice(selection);
-  console.log(playRound(playerSelection, computerSelection));
-  console.log(computerSelection);
-  console.log(playerScore, computerScore);
+  showWinner(playerSelection, computerSelection);
+  computerComment.innerHTML = `Computer picked ${computerSelection}`;
+  updateScores();
 };
 
-//Game
-const game = () => {
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
+const showWinner = (a, b) => {
+  winnerComment.innerHTML = playRound(a, b);
 };
 
-//Values
+const updateScores = () => {
+  pScore.innerHTML = playerScore;
+  cScore.innerHTML = computerScore;
+};
+
+const endGame = () => {
+  rockBtn.disabled = true;
+  paperBtn.disabled = true;
+  scissorsBtn.disabled = true;
+  overlay.style.display = "block";
+};
+
+const restartGame = () => {
+  playerScore = 0;
+  computerScore = 0;
+  pScore.innerHTML = "0";
+  cScore.innerHTML = "0";
+  rockBtn.disabled = false;
+  paperBtn.disabled = false;
+  scissorsBtn.disabled = false;
+  overlay.style.display = "none";
+};
